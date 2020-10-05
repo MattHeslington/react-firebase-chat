143a441eaa3c108c340c0d606799f206a1482828
Matt Heslington <matt.heslington@icloud.com>
2020-10-05 12:45:12 +0800

    💄 Style changes

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <h1>⚛️🔥💬</h1>
+                <h1>Messenger</h1>
                 <SignOut />
             </header>
             <section>
